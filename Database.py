02825ca6299c65b26fdc7d9b3bd178e8ff0e3ecf
Mario Taddeucci <taddeucci23@gmail.com>
2018-10-08 21:57:36 -0300

    Rest Exame

--- Database.py
+++ Database.py
@@ -12,6 +12,7 @@ class Exame(Model):
     descricao = Column(String(255), nullable=False)
     duracao = Column(Integer, nullable=False)
     ac = Column(Boolean, nullable=False)
+    restricoes = relationship("ExameRestricao", back_populates="exame")
 
 
 class Restricao(Model):
@@ -29,12 +30,19 @@ class RestricaoMsg(Model):
 
 class ExameRestricao(Model):
     __tablename__ = 'exame_restricao'
-    id_exame = Column(Integer, primary_key=True)
+    id_exame = Column(Integer, ForeignKey(Exame.__tablename__ + '.id'))
     id_restricao = Column(Integer, primary_key=True)
-    tempo_restricao = Column(String(255), nullable=False)
+    tempo_restricao = Column(Integer, nullable=False)
     executar = Column(Boolean, nullable=False)
 
+    exame = relationship("Exame", back_populates="restricoes")
 
-engine = create_engine('sqlite:///database.db')
+    __table_args__ = (
+        ForeignKeyConstraint([executar, id_restricao], [RestricaoMsg.executar, RestricaoMsg.id_restricao]),
+        UniqueConstraint('id_exame', 'id_restricao'),
+        {})
+
+
+engine = create_engine('sqlite:///databse.db')
 Model.metadata.create_all(engine)
 db_session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask_restless import APIManager
-from flask import Flask
+from flask import Flask, jsonify
 from Database import db_session, Exame
 
 app = Flask(__name__)
@@ -7,13 +7,25 @@ app.config['SECRET_KEY'] = "key"
 app.config['DEBUG'] = True
 crud = ['GET', 'POST', 'PATCH', 'DELETE']
 
-manager = APIManager(app, session=db_session)
-manager.create_api(Exame, methods=['GET'])
-
 
 @app.teardown_appcontext
 def cleanup(resp_or_exc):
     db_session.remove()
+
+
+@app.route('/api/exame')
+def rest_exame():
+    dt = db_session.query(Exame.id, Exame.nome, Exame.ac).all()
+    dt = [{'id': d[0], 'name': d[1], 'ac': bool(d[2])} for d in dt]
+    return jsonify(dt)
+
+
+@app.route('/api/roteiro')
+def roteiro():
+    dt = db_session.query(Exame).all()
+    for d in dt:
+        print(d)
+    return 'teste'
 
 
 if __name__ == '__main__':
